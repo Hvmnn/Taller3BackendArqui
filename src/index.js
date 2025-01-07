81606b9af2aaf97e1fd84833479e34a93ff85cf3
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-07 19:09:07 -0300

    fix: update database connection.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,27 @@ const main = () => {
   console.log("Server on port 4000 - New Version", app.get("port"));
 };
 
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión a la base de datos exitosa.");
+  } catch (error) {
+    console.error("Error al conectar con la base de datos:", error);
+  }
+};
+
+testConnection();
 main();
